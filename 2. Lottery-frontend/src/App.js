553c1ba2a9772f891656384de80430b790832cc4
vaggy <sarvagya6@gmail.com>
2021-12-30 02:02:08 -0500

    lottery

--- 2. Lottery-frontend/src/App.js
+++ 2. Lottery-frontend/src/App.js
@@ -3,9 +3,13 @@ import web3 from "./web3";
 import lottery from "./lottery";
 
 class App extends React.Component {
-  
-  state = { manager: "", players: [], balance: "", value: "", message: "" };
-
+  state = {
+    manager: "",
+    players: [],
+    balance: "",
+    value: "",
+    message: "",
+  };
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
     const players = await lottery.methods.getPlayers().call();
@@ -14,7 +18,7 @@ class App extends React.Component {
     this.setState({ manager, players, balance });
   }
 
-  onSubmit = async event => {
+  onSubmit = async (event) => {
     event.preventDefault();
 
     const accounts = await web3.eth.getAccounts();
@@ -23,12 +27,11 @@ class App extends React.Component {
 
     await lottery.methods.enter().send({
       from: accounts[0],
-      value: web3.utils.toWei(this.state.value, "ether")
+      value: web3.utils.toWei(this.state.value, "ether"),
     });
 
     this.setState({ message: "You have been entered!" });
-
-  } 
+  };
 
   onClick = async () => {
     const accounts = await web3.eth.getAccounts();
@@ -36,21 +39,22 @@ class App extends React.Component {
     this.setState({ message: "Waiting on transaction success..." });
 
     await lottery.methods.pickWinner().send({
-      from: accounts[0]
+      from: accounts[0],
     });
 
-    this.setState({ message: "A winner has been picked!" }); 
+    this.setState({ message: "A winner has been picked!" });
   };
-
 
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
         <p>
-          There are currently {this.state.players.length} players competing to win {web3.utils.fromWei(this.state.balance, "ether")} ether!
+          This contract is managed by {this.state.manager}. There are currently{" "}
+          {this.state.players.length} people entered, competing to win{" "}
+          {web3.utils.fromWei(this.state.balance, "ether")} ether!
         </p>
+
         <hr />
         <form onSubmit={this.onSubmit}>
           <h4>Want to try your luck?</h4>
@@ -58,19 +62,20 @@ class App extends React.Component {
             <label>Amount of ether to enter</label>
             <input
               value={this.state.value}
-              onChange={event => this.setState({ value: event.target.value })}
+              onChange={(event) => this.setState({ value: event.target.value })}
             />
           </div>
           <button>Enter</button>
         </form>
+
         <hr />
 
         <h4>Ready to pick a winner?</h4>
         <button onClick={this.onClick}>Pick a winner!</button>
 
+        <hr />
+
         <h1>{this.state.message}</h1>
-
-
       </div>
     );
   }
